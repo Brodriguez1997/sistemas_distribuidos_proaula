721163909550d9dea5fff9816ebf234d0fe898e4
santiagojagua <80717176+santiagojagua@users.noreply.github.com>
2025-04-22 14:29:56 -0500

    agregar urlfuncion server

--- User_custom_ldap/Files/views.py
+++ User_custom_ldap/Files/views.py
@@ -47,6 +47,28 @@ def enviar_mensaje(mensaje):
 
     return response
 
+def recibir_url(url, nombre_url, tipo):
+    
+    url = "http://host.docker.internal:3000/api/pdf/"
+
+    channel = grpc.insecure_channel('host.docker.internal:50051')
+    stub = grpc_pb2_grpc.ConvertidorUrlsStub(channel)
+
+    request = grpc_pb2.ConvertirUrlsRequest(url=url, nombre=nombre_url, tipo=tipo)
+    response = stub.ConvertirUrls(request)
+    print(response)
+    
+    if response:
+        data = {
+            "nombre": nombre_url,
+            "ruta": 10,
+            "tamano": 1000,
+        }
+        response_db = requests.post(url, json=data)
+        print(response_db)
+
+    return "recibido"
+
 def recibir_archivo(file, nombre_archivo, tipo):
     
     url = "http://host.docker.internal:3000/api/pdf/"
