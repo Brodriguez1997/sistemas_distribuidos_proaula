5d5a81a48f76677b906370680c904a76e0211ae1
Brodriguez1997 <41337069+Brodriguez1997@users.noreply.github.com>
2025-04-22 08:43:23 -0500

    correccion 5

--- nodejs/servidor-api/modelo/modelo.js
+++ nodejs/servidor-api/modelo/modelo.js
@@ -3,34 +3,34 @@ const db = require('../configuracion/db.js');
 
 class PdfModel {
     static async getAll() {
-        const res = await db.query('SELECT * FROM archivos_pdf');
+        const res = await db.query('SELECT * FROM archivo');
         return res.rows;
     }
 
-    static async getById(id) {
-        const res = await db.query('SELECT * FROM archivos_pdf WHERE id = $1', [id]);
-        return res.rows[0];
+    static async getById(id) {  
+        const res = await db.query('SELECT * FROM archivo WHERE id = $1', [id]);
+        return res.rows[0];     
     }
 
-    static async create(nombre, ruta, tamaño) {
+    static async create(nombre, nodo, peso) {
         const fechaActual = new Date();
         const res = await db.query(
             'INSERT INTO archivo(nombre, nodo, peso, fecha) VALUES($1, $2, $3, $4) RETURNING *',
-            [nombre, ruta, tamaño, fechaActual]
+            [nombre, nodo, peso, fechaActual]
         );
         return res.rows[0];
     }
 
-    static async update(id, nombre, ruta, tamaño) {
+    static async update(id, nombre, nodo, peso) {
         const res = await db.query(
-            'UPDATE archivos_pdf SET nombre = $1, ruta = $2, tamaño = $3 WHERE id = $4 RETURNING',
-            [nombre, ruta, tamaño, id]
+            'UPDATE archivo SET nombre = $1, ruta = $2, tamaño = $3 WHERE id = $4 RETURNING',
+            [nombre, nodo, peso, id]
         );
         return res.rows[0];
     }
 
     static async delete(id) {
-        const res = await db.query('DELETE FROM archivos_pdf WHERE id = $1 RETURNING *', [id]);
+        const res = await db.query('DELETE FROM archivos WHERE id = $1 RETURNING *', [id]);
         return res.rows[0];
     }
 }
