1ad0a45077323f6b60c0614beb38c42032f466d6
Brodriguez1997 <41337069+Brodriguez1997@users.noreply.github.com>
2025-04-23 13:40:59 -0500

    pruebas
    falla con el soap

--- nodejs/servidor-api/configuracion/db.js
+++ nodejs/servidor-api/configuracion/db.js
@@ -4,7 +4,7 @@ const client = new Client({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: 'jagua',
+    password: 'Antoniors1997',
     database: 'convertidor_pdf',
 });
 
